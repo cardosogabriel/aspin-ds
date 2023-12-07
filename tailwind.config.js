e09c426a05bd97b526657558eaada9aac2f02973
Gabriel Cardoso <gabrielc.cardoso@gmail.com>
2023-12-07 17:50:11 +0300

    Creates button page with markup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        link: '#1A56DB', //blue-700
+        linkHover: '#1E429F', //blue-800
+        linkActive: '#233876', //blue-900
+        highEmphasis: '#1F2A37', //gray-800
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
